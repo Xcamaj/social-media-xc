eb5f3eec1663a0fdf0bbf111e856477c3396f818
Xcamaj <Xhemajlcamaj@gmail.com>
2022-05-16 17:47:09 -0400

    mongodb issue

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-    useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-media-xc', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
